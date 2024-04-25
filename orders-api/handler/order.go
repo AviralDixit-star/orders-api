c7c56e0cbfa1af89c6140482f7b062fa5157de21
AviralDixit-star <dixitprakhar915@gmail.com>
2024-04-25 23:43:49 +0530

    interface implemented

--- orders-api/handler/order.go
+++ orders-api/handler/order.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,15 +10,23 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 
 	"github.com/AviralDixit-star/orders-api/model"
 	"github.com/AviralDixit-star/orders-api/repository/order"
 )
 
+type Repo interface {
+	Insert(ctx context.Context, order model.Order) error
+	FindAll(ctx context.Context, page order.FindAllPage) (order.FindResult, error)
+	FindByID(ctx context.Context, id uint64) (model.Order, error)
+	DeleteByID(ctx context.Context, id uint64) error
+	Update(ctx context.Context, order model.Order) error
+}
+
 type Order struct {
-	Repo *order.RedisRepo
+	Repo Repo
 }
 
 func (h *Order) Create(w http.ResponseWriter, r *http.Request) {
